9b2320bfecf316a7e1d2bd0cf8b6a314e37d83b0
IplinskyLeo <leoipy16@gmail.com>
2022-09-15 11:28:01 -0300

    initial commit

---
+++ todo9/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import { Header } from './components/Header';
+
+import './global.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+     
+     <Header />
+
+    <h1>ta ok</h1>
+
+    </div>
+  )
+}
+
+export default App
---
+++ todo9/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+export function Header(){
+    return(
+        <header className='header'>
+            <p>Uma seleção de produtos</p>
+            <h1>Especial pra voce</h1>
+            <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+
+            <div>
+                <button>assss</button>
+            </div>
+
+        </header>
+    );
+
+}
