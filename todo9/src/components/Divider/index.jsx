b8dc4e476905e769102783bfac91759024a1c998
IplinskyLeo <leoipy16@gmail.com>
2022-09-17 17:27:44 -0300

    add: (divider)

--- todo9/src/components/Divider/index.jsx
+++ todo9/src/components/Divider/index.jsx
@@ -1,9 +1,20 @@
-
+import './index.css';
 
 export function Divider({ text}) {
     return (
-      <div >
-        <h3>{text}</h3>
+      <div className="todos">
+
+
+        <div className="div1"></div>
+
+        <div className="div2">
+          <h3>{text}</h3>
+        </div>
+
+        <div className="div3"></div>
+
+
       </div>
+
     );
   }
